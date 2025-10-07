9c1eacf78922e3a954e9fee2cce91a63ffed26af
風止渚 <220871631+kztmrngs@users.noreply.github.com>
2025-10-08 00:03:40 +0900

    パフォーマンス最適化

--- works/tools/diff/script.js
+++ works/tools/diff/script.js
@@ -5,8 +5,8 @@ window.onload = function () {
     var diffOutput = document.getElementById('diff-output');
     var diffModeRadios = document.getElementsByName('mode');
 
-    // 文字単位のLCSアルゴリズムによるdiff
-    function diffChars(a, b) {
+    // 汎用LCSアルゴリズム
+    function performLCS(a, b) {
         var aLen = a.length;
         var bLen = b.length;
         var matrix = [];
@@ -29,14 +29,14 @@ window.onload = function () {
         var i = aLen, j = bLen;
         while (i > 0 || j > 0) {
             if (i > 0 && j > 0 && a[i - 1] === b[j - 1]) {
-                result.unshift({ type: 'common', char: a[i - 1] });
+                result.unshift({ type: 'common', value: a[i - 1] });
                 i--;
                 j--;
             } else if (j > 0 && (i === 0 || matrix[i][j - 1] >= matrix[i - 1][j])) {
-                result.unshift({ type: 'added', char: b[j - 1] });
+                result.unshift({ type: 'added', value: b[j - 1] });
                 j--;
             } else if (i > 0 && (j === 0 || matrix[i][j - 1] < matrix[i - 1][j])) {
-                result.unshift({ type: 'removed', char: a[i - 1] });
+                result.unshift({ type: 'removed', value: a[i - 1] });
                 i--;
             } else {
                 break;
@@ -45,50 +45,19 @@ window.onload = function () {
         return result;
     }
 
-    // 行単位のLCSアルゴリズムによるdiff
+    // 文字単位のdiff
+    function diffChars(a, b) {
+        return performLCS(a.split(''), b.split(''));
+    }
+
+    // 行単位のdiff
     function diffLines(a, b) {
         var aLines = a.split(/\r\n|\r|\n/);
         var bLines = b.split(/\r\n|\r|\n/);
         // 末尾の空行を無視する
         if (aLines.length > 0 && aLines[aLines.length - 1] === '') aLines.pop();
         if (bLines.length > 0 && bLines[bLines.length - 1] === '') bLines.pop();
-
-        var aLen = aLines.length;
-        var bLen = bLines.length;
-        var matrix = [];
-
-        for (var i = 0; i <= aLen; i++) {
-            matrix[i] = new Array(bLen + 1).fill(0);
-        }
-
-        for (var i = 1; i <= aLen; i++) {
-            for (var j = 1; j <= bLen; j++) {
-                if (aLines[i - 1] === bLines[j - 1]) {
-                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
-                } else {
-                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
-                }
-            }
-        }
-
-        var result = [];
-        var i = aLen, j = bLen;
-        while (i > 0 || j > 0) {
-            if (i > 0 && j > 0 && aLines[i - 1] === bLines[j - 1]) {
-                result.unshift({ type: 'common', line: aLines[i - 1] });
-                i--;
-                j--;
-            } else if (j > 0 && (i === 0 || matrix[i][j - 1] >= matrix[i - 1][j])) {
-                result.unshift({ type: 'added', line: bLines[j - 1] });
-                j--;
-            } else if (i > 0 && (j === 0 || matrix[i][j - 1] < matrix[i - 1][j])) {
-                result.unshift({ type: 'removed', line: aLines[i - 1] });
-                i--;
-            } else {
-                break;
-            }
-        }
-        return result;
+        return performLCS(aLines, bLines);
     }
 
     function displayDiff() {
@@ -109,11 +78,13 @@ window.onload = function () {
             return;
         }
 
+        var fragment = document.createDocumentFragment();
+
         if (mode === 'line') {
             var lineDiffResult = diffLines(textA, textB);
             lineDiffResult.forEach(function(part) {
                 var span = document.createElement('span');
-                span.textContent = part.line + '\n';
+                span.textContent = part.value + '\n';
                 if (part.type === 'added') {
                     span.className = 'added';
                     span.textContent = '+ ' + span.textContent;
@@ -123,21 +94,22 @@ window.onload = function () {
                 } else {
                     span.textContent = '  ' + span.textContent;
                 }
-                diffOutput.appendChild(span);
+                fragment.appendChild(span);
             });
         } else { // char mode
             var charDiffResult = diffChars(textA, textB);
             charDiffResult.forEach(function(part) {
                 var span = document.createElement('span');
-                span.textContent = part.char;
+                span.textContent = part.value;
                 if (part.type === 'added') {
                     span.className = 'added';
                 } else if (part.type === 'removed') {
                     span.className = 'removed';
                 }
-                diffOutput.appendChild(span);
+                fragment.appendChild(span);
             });
         }
+        diffOutput.appendChild(fragment);
     }
 
     compareBtn.onclick = displayDiff;
